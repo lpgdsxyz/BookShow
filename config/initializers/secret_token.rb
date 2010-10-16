7dc46d057ca8b326c5c1b124c905c18e7033f943
lipei <1036129755@qq.com>
2010-10-17 01:39:54 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookShow::Application.config.secret_token = 'b3498ba5c55b54a2c256c4431e56dd9a3d51972eac00e180e4fc7e452ac4a839d9f611807749adeea3b90db63da2c02a908b9b4d25a65cd0b2619b1f491c9be6'
